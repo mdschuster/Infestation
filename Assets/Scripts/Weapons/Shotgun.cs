58de2503f2bd8d8c6fc207b85b8790ad45fb0342
Micah Schuster <micahdschuster@gmail.com>
2022-04-10 20:35:21 -0400

    Add shotgun with animations, particle effects, and wall hit effects.

---
+++ Assets/Scripts/Weapons/Shotgun.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : MonoBehaviour
+{
+
+    public float timeBetweenShots;
+    private float time;
+
+    private Animator animator;
+    public LayerMask layerMask;
+    public ParticleSystem shootEffect;
+    public ParticleSystem wallHitEffect;
+
+    private RaycastHit rayHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        time-=Time.deltaTime;
+        if(time<=0 && Input.GetMouseButtonDown(0)) {
+            time = timeBetweenShots;
+            animator.SetTrigger("Shoot");
+            shootEffect.Play();
+            //animator.SetBool("Shoot", false);
+            bool hit = Physics.Raycast(transform.position, transform.forward,out rayHit,100f,layerMask);
+            if (hit) {
+                if (rayHit.collider.tag == "Wall") {
+                    Quaternion rotation = Quaternion.LookRotation(rayHit.normal, Vector3.up);
+                    Instantiate(wallHitEffect, rayHit.point, rotation);
+                }
+            }
+
+        }
+
+    }
+}
